f5e2e383f1a18a9ba9032f06ad90031fec0ea873
ci-a <73971286+ci-a@users.noreply.github.com>
2023-02-10 16:09:33 -0500

    Update HourlyEmployee.java

--- src/main/java/testPrep1/HourlyEmployee.java
+++ src/main/java/testPrep1/HourlyEmployee.java
@@ -1,16 +1,10 @@
-package testPrep1;
-
-public class HourlyEmployee extends Employee {
-
-	public HourlyEmployee(String name, String LN, float r) {
-		super(name, LN, r, "Hourly Employee");
-	}
-	
-	public float pay(){
-		float p1 = super.getHours() > 40 ? 40 : super.getHours();
-		float p2 = super.getHours() - p1;
-		super.setLastPayment(p1 * super.getRate() + p2 * 1.5f * super.getRate());
-		return super.getLastPayment();        
-	}                              
-
-}
+			        package testPrep1;public class HourlyEmployee 
+                             extends              Employee       {public HourlyEmployee
+			/*sadfacesad*/		(String 	name, //sadfacesadface
+			   String LN,            float.         r) {super(name, LN,
+			r, "Hourly Employee");}public float pay(){float p1 = super.getHours( 
+			   )  >  40  ?  40  :              super.getHours();float p1 = super.getHours() > 
+			      40 ? 40 :                            super.getHours();
+			    float p2 =     super.getHours() - p1;       super.setLastPayment(p1 * 
+				 super.getRate() + p2 * 1.5f * super.getRate()
+				    );return super.getLastPayment();}}
