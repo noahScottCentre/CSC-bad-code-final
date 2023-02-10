4a52ce7db97185333f9ec0cd046c4a1371804a83
Karli Prather <106628367+K-Prather@users.noreply.github.com>
2023-02-10 15:47:55 -0500

    Weird nesting

--- src/main/java/testPrep1/Contractors.java
+++ src/main/java/testPrep1/Contractors.java
@@ -1,7 +1,9 @@
 package testPrep1;
 
 public class Contractors extends Employee {
-	public Contractors(String FN, String LN, float R) {
-		super(FN, LN, R, "Contractor");
-	}
+	public 
+		Contractors(String FN, String LN, float R)
+							 {super
+							  (FN, LN, R, "Contractor");
+							 }
 }
