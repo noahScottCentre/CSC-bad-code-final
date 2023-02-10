199e8d2211110298c198c20e7f6d8ded5a40d1e8
noahScottCentre <124309801+noahScottCentre@users.noreply.github.com>
2023-02-10 16:01:27 -0500

    Update Employee.java

--- src/main/java/testPrep1/Employee.java
+++ src/main/java/testPrep1/Employee.java
@@ -7,11 +7,11 @@
 		       this.l          = l;}public          void setRate(float r) { 
 		       this.r          = r;}public          void setHours(float h){ 
 		       this.h          = h;}public          void setLastPayment(float
-		       p){ this.p = p;} public String getFirstName() {     return f; } 
-			public String getLastName () { return l; }        public float
-		         getRate()                                      { return r; }           
-                          public float                             getHours(){return 
-		            h; } public float getLastPayment(){ return p; } public
-                              float pay() { p = r * h; return p; } public String 
-                                toString() { return this.t + ": " + this.f + " " 
-                                   + this.l + " Last Payment: " + this.p;}}
+		       p){ this.p = p;} public String getFirstName(){      return f;} 
+			public String getLastName () { return l;}       public  float
+		         getRate()                                     { return r; }           
+                          public float                            getHours(){return 
+		            h; } public float          getLastPayment(){return p;}  
+			      public float pay() { p = r * h; return p;} public 
+			       String toString(){ return this.t +": "+this.f+
+				  " "+this.l + " Last Payment: "+this.p;}}
