24c2e47ba885d7f9acb6a4d119110116ed16ba01
Karli Prather <106628367+K-Prather@users.noreply.github.com>
2023-02-10 15:56:36 -0500

    Update Contractors.java

--- src/main/java/testPrep1/Contractors.java
+++ src/main/java/testPrep1/Contractors.java
@@ -1,9 +1,6 @@
-package testPrep1;
-
-public class Contractors extends Employee {
-	public 
-		Contractors(String FN, String LN, float R)
-							 {super
-							  (FN, LN, R, "Contractor");
-							 }
-}
+package testPrep1; public class Contractors extends Employee 
+	{public Contractors(
+		String FN, String LN, float R)
+		{super(
+	FN, LN, R, 
+"Contractor");}}
